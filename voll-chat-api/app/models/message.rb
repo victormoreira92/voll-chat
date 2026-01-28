2e3c34c11254ffe7e0efb9d732dea553669d16d7
Victor Hugo Moreira <vhmrj92@gmail.com>
2026-01-28 16:42:50 -0300

    feat: Add attached e broadcast_message

--- voll-chat-api/app/models/message.rb
+++ voll-chat-api/app/models/message.rb
@@ -1,6 +1,11 @@
 class Message < ApplicationRecord
+  has_one_attached :image
   belongs_to :sender, class_name: 'User'
   belongs_to :receiver, class_name: 'User'
+  after_commit :broadcast_message, on: :create
 
-  validates :content, presence: true
+  def broadcast_message
+    BroadcastMessageJob.perform_async(self.id)
+  end
+
 end
