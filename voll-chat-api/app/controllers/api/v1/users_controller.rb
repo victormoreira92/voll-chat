cb10a28c220bc9a46bb3ba8456ed9968d15aface
Victor Hugo Moreira <vhmrj92@gmail.com>
2026-01-28 00:34:28 -0300

    feat: Add user_params no controller

--- voll-chat-api/app/controllers/api/v1/users_controller.rb
+++ voll-chat-api/app/controllers/api/v1/users_controller.rb
@@ -4,17 +4,22 @@ module Api
       skip_before_action :authenticate
 
       def login
-        user = User.find_by(email: params[:email])
-        authenticated_user = user&.authenticate(params[:password])
+        user = User.find_by(email: user_params[:email])
+        authenticated_user = user&.authenticate(user_params[:password])
 
         if authenticated_user
           token = JsonTokenWeb.encode(user_id: user.id)
           expires_at = JsonTokenWeb.decode(token)[:exp]
 
-          render json: { name: user.name, token: token, expires_at: expires_at }, status: :ok
+          render json: { name: user.name, user_id: user.id, token: token, expires_at: expires_at }, status: :ok
         else
           render json: { message: I18n.t("errors.invalid_credentials") }, status: :unauthorized
         end
+      end
+
+    private
+      def user_params
+        params.require(:user).permit(:email, :password)
       end
     end
   end
