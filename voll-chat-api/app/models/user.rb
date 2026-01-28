4c1ea7643a37c4b0a4d60b82465c446c28db04cf
Victor Hugo Moreira <vhmrj92@gmail.com>
2026-01-27 23:08:43 -0300

    feat: Relação de Messsage com User no model

--- voll-chat-api/app/models/user.rb
+++ voll-chat-api/app/models/user.rb
@@ -2,4 +2,14 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, uniqueness: true
+  has_many :sent_messages,
+           class_name: 'Message',
+           foreign_key: :sender_id,
+           dependent: :destroy
+
+  has_many :received_messages,
+           class_name: 'Message',
+           foreign_key: :receiver_id,
+           dependent: :destroy
+
 end
